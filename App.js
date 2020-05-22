4e5874e91eb837b8afb1520e82dd89660761b5f3
g4m3r2 <choicharles31@gmail.com>
2020-05-22 16:24:40 -0700

    final

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,KeyboardAvoidingView, Platform, TextInput, ImageBackground } from 'react-native';
+//keyboardavoiding view allows auto-avoidance of keyboard and can be used instead of view
+//note that must also specify behavior in KAV below
+import SearchInput from "./components/SearchInput"
+import getImageForWeather from './utils/getImageForWeather'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  render () {
+  const location = 'San Francisco'
+    //Now we must have a way for child component to retrieve the inputted location and communciate this to the App
+    //we must put render () on top because we must define and pass in {location} within the return
+    return (
+    
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"> 
+          <ImageBackground 
+            source={getImageForWeather('Clear')}
+            style={styles.imageContainer}
+            imageStyle={styles.image}/>
+            <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>24</Text>
+          <SearchInput
+            autoCorrect = {false}
+            placeholder = "Search any city"
+            placeholderTextColor="white"
+            style = {styles.textInput}
+            clearButtonMode="always" />
+            </View>
+            
+      </KeyboardAvoidingView>
+
+
+      //clearbuttonmode is only for ios
+    );
+  }
 }
+/*Above, App: parent; SearchInput: child. 
+<Imagebackground: a builtin compnent that is basically <View> <Image>.
+We set its source to 
+Note we can put <view> inside <KAV> although both are views(?)
+ */
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //we must comment out the AI and JC above such that the background image will take up the entire space
   },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily:Platform.OS === 'ios' ? 'AvenirNext-Regular': 'Roboto',
+    color: 'white'
+  },
+  largeText: {
+    fontSize:44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+    imageContainer: {
+      flex:1,
+//rmb flex:1 means the component will take up any remaining room relative to sibling omponents
+// since IB is the only nested element within KAV, this means it will take up the whole space
+//the container is like view, and image is a component within that view.. so both must take up flex=1?
+    },
+  image: {
+    flex:1,
+    width:null,
+    height:null,
+    resizeMode: 'cover',
+    //by default, images have width and height = to that of source image. so we must disable it to fill it up
+
+    //resizemode: allows us to resize the pic to fit the component size
+    
+  },
+  detailsContainer: {
+    flex:1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  }
+  //this allows the container in imagebackground to fill up entire space of parent component and adds a semi-transparent overlay 
+
+
+
 });
---
+++ components/SearchInput.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import {View, TextInput, StyleSheet} from 'react-native'
+
+export default class SearchInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text:'',
+        }
+    }
+
+    /*recall that a class creates an object of serachinput with the property of props
+    and super() is required in derived classes in order to reference this within
+the constructor.
+    Much like how we can access the component’s props with this.props, we can access the component’s
+state via this.state. For example if we wanted to output our state property in a single Text
+component, we could do this
+class SearchInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text:'Hi there!',}};
+    render() {
+        return <Text>{this.state.text}</Text>
+    }
+    */
+    handleChangeText=(text)=> {
+        this.setState({text})
+    }
+    
+    
+    render () {
+        const {placeholder} = this.props;
+        const {text} = this.state;
+        /*above, we destructureboth objects into individual variables called placeholder and text instead of using
+        state such as this.state and this.props.placeholder*/
+        return (
+            <View style={styles.container}>
+                <TextInput 
+                    autoCorrect = {false}
+                    value ={text}
+                    placeholder={placeholder}
+                    placeholderTextColor="white"
+                    underlineColorAndroid="transparent"
+                    style={styles.textInput}
+                    clearButtonMode ="always"
+                    onChangeText={this.handleChangeText}
+                    onSubmitEditing={this.handleSubmitEditing}/>
+            </View>
+        /*underlinecolorandroid: in android, the default text will
+        have an underline. we get rid of it by doing this
+        
+        value: now represents {text}, which itself is = this.state
+        onSUbmitEditing: 
+        onChangeText: alows TextInput to pass in a function called handleChangeTExt wihc returns the inptted text as an argument
+        This is an example of a callback function
+        this allows the child (textinput) to notify the parent (searchinput) of the result the user has inputted
+        But because currently handlechangetext has a different local scope (rmb outisders don't have access to insider functions)
+        we mjust use property initializers. Ie defining handlechangetext as an arrow function. 
+
+        BUT we can't pass properties from child (searchinput) to app (parent). 
+        Properties are owned by parent to child, so must use state instead
+        */
+
+        )
+    }
+}
+const styles = StyleSheet.create ({
+    container: {
+         height: 40,
+         marginTop: 20,
+        backgroundColor: '#666',
+        marginHorizontal: 40,
+        paddingHorizontal: 10,
+         borderRadius: 5,
+    },
+    textInput: {
+        flex:1,
+        color:'white',
+    },
+});
+
